93e064e6e540746079d569c72843b61a1dff4b38
Quentin Haerinck <quentin.haerinck@gmail.com>
2020-06-21 14:17:36 +0200

    verbetering scrambler

--- les-7/word-scrambler/script.js
+++ les-7/word-scrambler/script.js
@@ -1,3 +1,5 @@
+
+/*
 const buttonScramble = document.getElementById('submit-btn');
 let userInput = document.getElementById('user-input');
 let resultContainer = document.getElementById('result-container');
@@ -53,4 +55,77 @@ function realtime() {
 }
 
 userInput.addEventListener('input', realtime);
+
+///////////////////////////////////////////////////////////////
+*/
+let userInput = document.getElementById('user-input');
+let resultContainer = document.getElementById('result-container');
+let wordCountContainer = document.getElementById('word-count');
+let letterCountContainer = document.getElementById('letter-count');
+let submitBtn = document.getElementById('submit-btn');
+
+function getUserInput(){
+    return userInput.value;
+}
+
+function textToWordArray(text){
+    return text.split(" ");
+}
+
+function arrayToText(array){
+    return array.join(' ');
+}
+
+function getRandomNumber(max){
+    return Math.floor(Math.random() * (max));
+}
+
+
+function updateLetterCount(text){
+    letterCountContainer.innerHTML = text.length;
+}
+
+function updateWordCount(array){
+    wordCountContainer.innerHTML = array.length;
+}
+
+
+function scrambleArray(oldArray){
+    /*
+    for (let i = oldArray.length - 1; i >= 0; i--){
+        let randomIndex = getRandomNumber(i);
+        let randomValue = oldArray[randomIndex];
+        let arrayvalue = oldArray[i];
+        oldArray[i] = randomValue;
+        oldArray[randomIndex] = arrayvalue;
+    }
+    */
+    const scrambledArray = [];
+    while(oldArray > 0){
+        const randomIndex = getRandomNumber(oldArray.length);
+        const randomValue = oldArray.splice(randomIndex, 1);
+        scrambleArray.push(randomValue[0]);
+    }
+    return scrambleArray;
+}
+
+function scrambleText(text){
+    let array = textToWordArray(text);
+    updateWordCount(array);
+    let scrambled = scrambleArray(array);
+    return arrayToText(scrambled);
+}
+
+function scramble(){
+    let text = getUserInput();
+    let scrambledText = scrambleText(text);
+    resultContainer.innerHTML = scrambledText;
+
+    updateLetterCount(text);
+}
+
+
+submitBtn.addEventListener('click', scramble);
+userInput.addEventListener('input', scramble);
+
 

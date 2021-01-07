2849872208901f734bdc2292286cf6b160626cfe
QuentinHck <quentin.haerinck@gmail.com>
2021-01-07 19:38:59 +0100

    newTodo

--- les-16-fetch/Todo/script-newTodo.js
+++ les-16-fetch/Todo/script-newTodo.js
@@ -1,9 +1,15 @@
 const $listContainer = document.querySelector('.content-list');
 const $buttonAddTodo = document.querySelector('.button-add');
 const $modal = document.getElementById('myModal');
-const $modalCloseBtn = document.querySelector('.modal-close');
+const $modalResetBtn = document.querySelector('.reset-btn');
+const $todoForm = document.forms['todo-form'];
+
+let allTodoItems = {};
 
 function fetchAllTodos() {
+
+  $listContainer.classList.add('loading');
+
   fetch('https://phpstack-224488-1624928.cloudwaysapps.com/_/items/todo', {
     method: 'GET',
     headers: {
@@ -17,11 +23,14 @@ function fetchAllTodos() {
       return result.json();
     })
     .then(function(result) {
-      console.log(result.data)
+      console.log(result.data);
+      saveTodoList(result.data);
       printAllTodos(result.data,true);
+      $listContainer.classList.remove('loading');
     })
     .catch(function(error){
-      console.error(error)
+      console.error(error);
+      $listContainer.classList.remove('loading');
     });
 }
 
@@ -63,31 +72,137 @@ function deleteTodo(id) {
     });
 }
 
-function deleteBtnOnClick(event){
-  if(event.target.matches('.button-delete')){
-    const $listItem = event.target.closest('.content-list__item');
-    const id = $listItem.dataset.id;
-    deleteTodo(id);
-  }
+function postNewTodo(body) {
 
-  if(event.target.matches('.button-update')){
-    const $listItem = event.target.closest('.content-list__item');
-    const id = $listItem.dataset.id;
-    openModal();
+  $modal.classList.add('loading');
+
+  fetch('https://phpstack-224488-1624928.cloudwaysapps.com/_/items/todo', {
+    method: 'POST',
+    headers: {
+      Authorization: 'bearer ABcEHA2kcrKY4a6ipUA3',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((result) => {
+      if (!result.ok) {
+        throw new Error('No luck');
+      }
+
+      return result.json(); // JSON.parse
+    })
+    .then(function (result) {
+      console.log(result);
+      fetchAllTodos();
+      $todoForm.reset();
+      $modal.classList.remove('loading');
+    })
+    .catch(function (error) {
+      console.error(error);
+      $modal.classList.remove('loading');
+    });
+}
+
+function updateTodo(id,body){
+  
+  fetch('https://phpstack-224488-1624928.cloudwaysapps.com/_/items/todo/' + id, {
+    method: 'PATCH',
+    headers: {
+      'Authorization': 'bearer ABcEHA2kcrKY4a6ipUA3',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+  .then(result => {
+    if (!result.ok) {
+      throw new Error('No luck');
+      }
+      return result.json(); // JSON.parse
+    })
+  .then(function(result) {
+    console.log(result)
+    fetchAllTodos();
+    $todoForm.reset();
+  })
+  .catch(function(error){
+    console.error(error)
+  });
+
+}
+
+function todoItemFromForm(){
+  return{
+    description: $todoForm.elements.description.value,
   }
 }
 
-function openModal(){
+function saveTodoList(todoItems,clear){
+  if(clear){
+    todoItems = {};
+  }
+  todoItems.forEach(function(todoItem){
+    allTodoItems[todoItem.id] = todoItem;
+  })
+}
+
+
+function openModal(todoItem){
   $modal.style.display = 'block';
+  console.log($todoForm.elements);
+  if(todoItem){
+    $todoForm.elements.id.value = todoItem.id;
+    $todoForm.elements.description.value = todoItem.description;
+  }
+
 }
 
 function closeModal(){
   $modal.style.display = 'none';
 }
 
-$buttonAddTodo.addEventListener('click',openModal);
-$modalCloseBtn.addEventListener('click',closeModal);
-$listContainer.addEventListener('click',deleteBtnOnClick);
+
+function deleteBtnOnClick(event){
+  const $listItem = event.target.closest('.content-list__item');
+  const id = $listItem.dataset.id;
+  deleteTodo(id);
+}
+
+function updateBtnClick(event){
+  const $listItem = event.target.closest('.content-list__item');
+  const id = parseInt($listItem.dataset.id);
+  openModal(allTodoItems[id]);
+}
+
+function listContainerClick(event){
+  if(event.target.matches('.button-delete')){
+    deleteBtnOnClick(event);
+  }
+  if(event.target.matches('.button-update')){
+    updateBtnClick(event);
+  }
+}
+
+function submitTodoForm(event){
+  event.preventDefault(event);
+  const id = $todoForm.elements.id.value;
+  const body = todoItemFromForm();
+
+  if(id){
+    updateTodo(id,body);
+  }else{
+    postNewTodo(body);
+  }
+
+  closeModal();
+}
+
+function addBtnCLick(){
+  openModal();
+}
 
 fetchAllTodos();
 
+$listContainer.addEventListener('click',listContainerClick);
+$buttonAddTodo.addEventListener('click',addBtnCLick);
+document.forms['todo-form'].addEventListener('submit',submitTodoForm);
+$modalResetBtn.addEventListener('click',closeModal);
